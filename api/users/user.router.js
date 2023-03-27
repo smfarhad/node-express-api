a32db2dc4f47001b1cced6e16aa7f6b376980599
smfarhad <farhad1556@gmail.com>
2023-03-27 11:21:34 +0600

    auth middleware apply

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,11 +1,12 @@
 const {createUser,getUsers,getUserById,updateUser,deleteUser,login} = require("../users/user.controller");
 const router = require("express").Router();
+const {checkToken} = require("../../auth/token_validation");
 
-router.post("/",createUser);
-router.get("/",getUsers);
-router.get("/:id",getUserById);
-router.patch("/",updateUser);
-router.delete("/",deleteUser);
+router.post("/",checkToken,createUser);
+router.get("/",checkToken,getUsers);
+router.get("/:id",checkToken,getUserById);
+router.patch("/",checkToken,updateUser);
+router.delete("/",checkToken,deleteUser);
 router.post("/login",login);
 
 module.exports = router;
